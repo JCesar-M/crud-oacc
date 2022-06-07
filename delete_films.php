918b1374c961119e97fa65475e363ea0a1de1307
Cesar <juliozaaks@gmail.com>
2022-06-06 19:29:36 -0500

    Modificando notificaciones

--- delete_films.php
+++ delete_films.php
@@ -10,13 +10,8 @@ if(isset($_GET['id'])) {
     die("Query Failed.");
   }
 
-<<<<<<< HEAD
-  $_SESSION['message'] = 'Películas eliminadas con éxito!!!';
+  $_SESSION['message'] = 'Películas eliminadas con éxito';
   $_SESSION['message_type'] = 'danger';
-=======
-  $_SESSION['message'] = 'Pelicula eliminada Exitosamente !!!';
-  $_SESSION['message_type'] = 'CUIDADO !!!';
->>>>>>> b142a4b36907ad6026dd508bb21b0fd753642e07
   header('Location: index.php');
 }
 
